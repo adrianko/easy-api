e8e9591f7721dda86abd35417a6530fc5084c1e8
Adrian Ko <adrianchko@gmail.com>
2015-06-06 20:24:56 +0100

    Initial commit

---
+++ src/main/Server.scala
@@ -0,0 +1,27 @@
+package main
+
+import java.net.{InetAddress, InetSocketAddress}
+import java.util.logging.Logger
+
+import com.sun.net.httpserver.HttpServer
+
+object Server {
+  
+  val nic: String = "0.0.0.0"
+  val port: Int = 9090
+  private val server: HttpServer = HttpServer.create(new InetSocketAddress(InetAddress.getByName(nic), port), 0)
+  private val logger: Logger = Logger.getLogger(this.getClass.toString)
+
+  def Server(): Unit = {
+    logger.info("Starting server on " + nic + ":" + port)
+    server.setExecutor(null)
+    server.start()
+  }
+  
+  def stop(): Unit = server.stop(0)
+  
+  def main(args: Array[String]): Unit = {
+    Server()
+  }
+  
+}
