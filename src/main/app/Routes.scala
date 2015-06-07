657ac41a17ca71d45e18dcbaa8adfd987195f6d3
AK <adrianchko@gmail.com>
2015-06-07 12:55:15 +0100

    Add second endpount

--- src/main/app/Routes.scala
+++ src/main/app/Routes.scala
@@ -1,10 +1,15 @@
 package main.app
+
 import main.core.Endpoint
 
 object Routes {
 
   object Settings extends Endpoint {
     def main(): String = "main"
+  }
+
+  object Another extends Endpoint {
+    def hello(): String = "world"
   }
 
 }
